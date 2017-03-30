d89caaffd1782ca140053ffac1209c1144cb3860
Alvaro Sanchez <heyalvaro@gmail.com>
2017-03-29 21:01:35 -0600

    Added music manager and some audio clips to enable bgm on the different levels

---
+++ Assets/scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destroy on load: " + name);
+    }
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+        Debug.Log("Playing Clip: " + thisLevelMusic);
+        if (thisLevelMusic) //If there's music attahced
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
